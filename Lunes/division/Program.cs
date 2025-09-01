936ab650eea7c0c07e317031494db34175b0e23f
FICT1 <ficallejas@uamv.edu.ni>
2025-09-01 08:58:08 -0600

    Lunes

---
+++ Lunes/division/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1, num2;
+float division; 
+
+Console.Write("Digite un numero: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite otro numero: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+division = (float)num1 / (float)num2;
+
+Console.WriteLine($"{num1} / {num2} es igual a {division}");
+
---
+++ Lunes/division2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int num1, num2;
+int division=0;
+Console.Write("Digite un numero: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite otro numero: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    division = num1 / num2;
+    Console.WriteLine($"{num1} / {num2} = {division}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("No se puede dividir entre 0");
+}
